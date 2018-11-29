e13039658ed4b85f6e1b736459048ecddcad79ef
FFang <564490014@qq.com>
2018-11-29 16:30:53 +0800

    2

---
+++ Assets/CameraControll.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    public Transform Target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(Target,Vector3.forward);
+	}
+}
